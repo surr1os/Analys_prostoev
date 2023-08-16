2bb43b5c2c54348e1eff0d3eed7f801d4b1b9cb3
Konstantin.kazakov <kostya.kazakov.30001@mail.ru>
2023-08-16 16:12:47 +0500

    Изменения: 1)Добавлена выгрузка таблицы из PostrgreeSQL(table analysis) 2)Добавлена фильтрация по датам(от/до) 3)Добавлена фильтрация по участку(поле region в таблице hpt_select)

--- Analys_prostoev/MainWindow.xaml.cs
+++ Analys_prostoev/MainWindow.xaml.cs
@@ -23,15 +23,35 @@ namespace Analys_prostoev
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string connectionString = "Host=10.241.224.71;Port=5432;Database=planning_dept_db;Username=postgres_10_241_224_71;Password=feDoz5Xreh";
+        // private string connectionString = "Host=10.241.224.71;Port=5432;Database=planning_dept_db;Username=postgres_10_241_224_71;Password=feDoz5Xreh";
+        private string connectionString = "Host=localhost;Database=myDb;Username=postgres;Password=iqdeadzoom1r";
+
 
         public MainWindow()
         {
             InitializeComponent();
         }
         //DataGridTable.ItemsSource
-       
-     
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT region FROM hpt_select";
+                using (NpgsqlCommand selectCommand = new NpgsqlCommand(selectQuery, connection))
+                {
+                    using (NpgsqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            selectComboBox.Items.Add(reader["region"].ToString());
+                        }
+                    }
+                }
+            }
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -39,8 +59,36 @@ namespace Analys_prostoev
             {
                 connection.Open();
 
-                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM analysis", connection))
+                string queryString = "SELECT * FROM analysis WHERE 1=1";
+                List<NpgsqlParameter> parameters = new List<NpgsqlParameter>();
+
+                if (startDatePicker.SelectedDate != null)
                 {
+                    queryString += " AND TO_DATE(\"Date_start\", 'DD-MM-YYYY') >= @startDate";
+                    parameters.Add(new NpgsqlParameter("startDate", NpgsqlTypes.NpgsqlDbType.Date));
+                    parameters[parameters.Count - 1].Value = startDatePicker.SelectedDate;
+                }
+
+                if (endDatePicker.SelectedDate != null)
+                {
+                    queryString += " AND TO_DATE(\"Date_start\", 'DD-MM-YYYY') <= @endDate";
+                    parameters.Add(new NpgsqlParameter("endDate", NpgsqlTypes.NpgsqlDbType.Date));
+                    parameters[parameters.Count - 1].Value = endDatePicker.SelectedDate;
+                }
+
+                if(selectComboBox.SelectedItem != null)
+                {
+                    string selectedRegion = selectComboBox.SelectedItem.ToString();
+                    queryString += $" AND region = @selectedRegion";
+                    parameters.Add(new NpgsqlParameter("selectedRegion", selectedRegion));
+                }
+
+               
+
+                using (NpgsqlCommand command = new NpgsqlCommand(queryString, connection))
+                {
+                    command.Parameters.AddRange(parameters.ToArray());
+
                     NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(command);
                     DataTable dataTable = new DataTable();
                     adapter.Fill(dataTable);
@@ -50,6 +98,7 @@ namespace Analys_prostoev
             }
         }
     }
+
 
      
 
