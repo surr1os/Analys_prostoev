703db90addb4b2c7197a2a613cc8dfe01d1f08fa
Konstantin.kazakov <kostya.kazakov.30001@mail.ru>
2023-08-16 13:07:46 +0500

    Добавьте файлы проекта.

---
+++ Analys_prostoev/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Analys_prostoev
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "Host=10.241.224.71;Port=5432;Database=planning_dept_db;Username=postgres_10_241_224_71;Password=feDoz5Xreh";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //DataGridTable.ItemsSource
+       
+     
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM analysis", connection))
+                {
+                    NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    DataGridTable.ItemsSource = dataTable.DefaultView;
+                }
+            }
+        }
+    }
+
+     
+
+
+    }
+
