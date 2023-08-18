224167768d8781704b4d76f23e663babb4a180f8
Константин Казаков <kostya.kazakov.30001@mail.ru>
2023-08-18 23:09:27 +0500

    Начальная структура реализации иерархических списков категорий и причин

---
+++ Analys_prostoev/CategoryHierarchy.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Analys_prostoev
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class CategoryHierarchy : Window
+    {
+        public CategoryHierarchy()
+        {
+            InitializeComponent();
+        }
+
+        // Реализация иерархического списка категорий и возвращение значения в ячейку
+    }
+}
