45beca9f6233c77fd52de13101a4ceadc4e08d11
Konstantin.kazakov <kostya.kazakov.30001@mail.ru>
2023-08-21 14:53:38 +0500

    двойной щелчок по ячейке из поля category открывает новое окно, где выбирается категория

--- Analys_prostoev/CategoryHierarchy.xaml.cs
+++ Analys_prostoev/CategoryHierarchy.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,11 +21,71 @@ namespace Analys_prostoev
     /// </summary>
     public partial class CategoryHierarchy : Window
     {
+        private string connectionString = "Host=localhost;Database=myDb;Username=postgres;Password=iqdeadzoom1r";
         public CategoryHierarchy()
         {
             InitializeComponent();
+
+
+            // Создаем модель представления
+           
+
         }
 
         // Реализация иерархического списка категорий и возвращение значения в ячейку
+
+        public class Category
+        {
+            public string CategoryName { get; set; }
+            public ObservableCollection<SubcategoryOne> SubcategoriesOne { get; set; }
+
+            public Category()
+            {
+                SubcategoriesOne = new ObservableCollection<SubcategoryOne>();
+            }
+        }
+
+        public class SubcategoryOne
+        {
+            public string SubcategoryOneName { get; set; }
+            public ObservableCollection<SubcategoryScnd> SubcategoriesScnd { get; set; }
+
+            public SubcategoryOne()
+            {
+                SubcategoriesScnd = new ObservableCollection<SubcategoryScnd>();
+            }
+        }
+
+        public class SubcategoryScnd
+        {
+            public string SubcategoryScndName { get; set; }
+            public ObservableCollection<SubcategoryThird> SubcategoriesThird { get; set; }
+
+            public SubcategoryScnd()
+            {
+                SubcategoriesThird = new ObservableCollection<SubcategoryThird>();
+            }
+        }
+
+        public class SubcategoryThird
+        {
+            public string SubcategoryThirdName { get; set; }
+        }
+
+        
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+           
+
+        }
     }
-}
+}     
+                    
+                
+            
+        
+    
+
--- Analys_prostoev/MainWindow.xaml.cs
+++ Analys_prostoev/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using Npgsql;
-using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using pree
+    using System.Windows.Controls;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 
 namespace Analys_prostoev
 {
@@ -97,11 +87,28 @@ namespace Analys_prostoev
                 }
             }
         }
+        private void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DataGridCellInfo cellInfo = DataGridTable.CurrentCell;
+
+            // Получаем столбец текущей ячейки
+            DataGridColumn column = cellInfo.Column;
+
+            // Проверяем, что столбец имеет заголовок "category"
+            if (column.Header.ToString() == "category")
+            {
+                // Открываем новое окно
+                var newWindow = new CategoryHierarchy();
+                newWindow.Show();
+            }
+        }
+
+    }
     }
 
 
      
 
 
-    }
+    
 
